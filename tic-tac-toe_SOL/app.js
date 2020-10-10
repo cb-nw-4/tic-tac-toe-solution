616dd035af3044e13d6e4eb151506a5153bfd29a
Tiffany D <tiffanyjdow@gmail.com>
2020-10-10 13:05:32 -0400

    Added solutions

---
+++ tic-tac-toe_SOL/app.js
@@ -0,0 +1,80 @@
+// tic tac toe
+
+// The HTML elements that we need for the game
+const board = document.querySelector('#board');
+const player1 = document.querySelector('.js-player-1');
+const player2 = document.querySelector('.js-player-2');
+const endMessageDiv = document.querySelector('.js-end-message');
+const restartBtn = document.querySelector('.restart');
+
+// this array tracks the state of the game board.
+const game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// Game Settings
+const PLAYER_1_ICON = 'X';
+const PLAYER_2_ICON = 'O';
+let COUNTER = 0; // track game progress
+
+// game starts with player 1
+let currentPlayer = '1';
+player1.classList.add('active');
+
+const handleRestart = () => {
+  restartBtn.removeEventListener('click', handleRestart);
+  location.reload();
+};
+
+const toggleRestartBtn = () => {
+  restartBtn.disabled = false;
+  restartBtn.addEventListener('click', handleRestart);
+};
+
+const win = () => {
+  board.removeEventListener('click', handleClick);
+  endMessageDiv.innerText = `Player ${currentPlayer} wins!`;
+  toggleRestartBtn();
+};
+
+const verifyWin = () => {
+  // ROWS
+  if (game[0] === game[1] && game[1] === game[2]) win();
+  if (game[3] === game[4] && game[4] === game[5]) win();
+  if (game[6] === game[7] && game[7] === game[8]) win();
+  // COLUMNS
+  if (game[0] === game[3] && game[3] === game[6]) win();
+  if (game[1] === game[4] && game[4] === game[7]) win();
+  if (game[2] === game[5] && game[5] === game[8]) win();
+  // DIAGONALS
+  if (game[0] === game[4] && game[4] === game[8]) win();
+  if (game[2] === game[4] && game[4] === game[6]) win();
+};
+
+const togglePlayer = () => {
+  currentPlayer = currentPlayer === '1' ? '2' : '1';
+  player1.classList.toggle('active');
+  player2.classList.toggle('active');
+};
+
+const handleClick = (event) => {
+  const cell = event.target.id;
+  const currentCellDiv = document.getElementById(cell);
+  const cellId = cell.charAt(cell.length - 1);
+  const icon = currentPlayer === '1' ? PLAYER_1_ICON : PLAYER_2_ICON;
+
+  if (typeof game[cellId] === 'number') {
+    currentCellDiv.innerText = icon;
+    // add value to game array
+    game[cellId] = icon;
+    COUNTER += 1;
+    if (COUNTER < 9) {
+      verifyWin();
+      togglePlayer();
+    } else {
+      endMessageDiv.innerText = `Draw!`;
+      toggleRestartBtn();
+    }
+  }
+  console.log(game);
+};
+
+board.addEventListener('click', handleClick);
